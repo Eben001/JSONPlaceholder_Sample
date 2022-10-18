3dccd5b954190089e9a4d690c79399eee6084d99
Gana Ebenezer <ebenezergana@gmail.com>
2022-10-18 23:47:19 +0100

    setup model for comments

---
+++ app/src/main/java/com/ebenezer/gana/jsonplaceholdersample/data/models/CommentItem.kt
@@ -0,0 +1,9 @@
+package com.ebenezer.gana.jsonplaceholdersample.data.models
+
+data class CommentItem(
+    val body: String,
+    val email: String,
+    val id: Int,
+    val name: String,
+    val postId: Int
+)
